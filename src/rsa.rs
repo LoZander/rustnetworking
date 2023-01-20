20810596689e2e35dc740142b0ec80d37e8208c6
Alexander <LoZander@outlook.com>
2023-01-20 16:00:18 +0100

    Encryption

--- src/rsa.rs
+++ src/rsa.rs
@@ -1,11 +1,11 @@
 use rand;
 use num_primes::{Generator, BigUint};
 
-type Message = String;
-type Cipher = String;
-
+type Message = Vec<u8>;
+type Cipher = Vec<u8>;
 pub struct PublicKey {
-    e: BigUint
+    e: BigUint,
+    n: BigUint
 }
 
 pub struct SecretKey {
@@ -19,6 +19,8 @@ pub enum Key {
 }
 
 pub fn generate(bit_size: u32) -> Result<(PublicKey,SecretKey),String> {
+    let e: BigUint = BigUint::new(vec![3]);
+
     let random_difference = rand::random::<f32>() * (bit_size as f32) / 10. as f32;
     let p_size = (bit_size as f32 / 2. + random_difference).floor() as usize;
     let q_size = (bit_size as f32 / 2. - random_difference).ceil() as usize;
@@ -39,15 +41,26 @@ pub fn generate(bit_size: u32) -> Result<(PublicKey,SecretKey),String> {
     }
 
     let (p,q) = f(p_size,q_size)?;
+    let n = p * q;
 
-    let public_key = PublicKey{e: BigUint::new(vec![3])};
+    let public_key = PublicKey{e,n};
     let secret_key = SecretKey{p,q};
     
     return Ok((public_key, secret_key))
 }
 
-pub fn encrypt(m: Message) -> Cipher {
-    todo!()
+pub fn encrypt(m: Message, pk: PublicKey) -> Cipher {
+    let m_number = number_from_bytes(m);
+    let cipher = m_number.modpow(&pk.e, &pk.n);
+    return cipher.to_bytes_be()
+}
+
+fn number_from_bytes(m: Message) -> BigUint {
+    return BigUint::from_bytes_be(&m)
+}
+
+fn bytes_from_number(c: BigUint) -> Cipher {
+    return c.to_bytes_be()
 }
 
 pub fn decrypt(c: Cipher) -> Message {
