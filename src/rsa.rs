71f010608ea8c75c2c5174d1dafafef50ef82510
Alexander D. Troelsen <LoZander@outlook.com>
2023-01-22 05:30:05 +0100

    Decryption except...
    Decryption except missing modular inverse in creation of d!

--- src/rsa.rs
+++ src/rsa.rs
@@ -3,8 +3,8 @@ use num_primes::{Generator, BigUint};
 
 type Message = Vec<u8>;
 type Cipher = Vec<u8>;
+const e: u32 = 3;
 pub struct PublicKey {
-    e: BigUint,
     n: BigUint
 }
 
@@ -19,8 +19,6 @@ pub enum Key {
 }
 
 pub fn generate(bit_size: u32) -> Result<(PublicKey,SecretKey),String> {
-    let e: BigUint = BigUint::new(vec![3]);
-
     let random_difference = rand::random::<f32>() * (bit_size as f32) / 10. as f32;
     let p_size = (bit_size as f32 / 2. + random_difference).floor() as usize;
     let q_size = (bit_size as f32 / 2. - random_difference).ceil() as usize;
@@ -43,7 +41,7 @@ pub fn generate(bit_size: u32) -> Result<(PublicKey,SecretKey),String> {
     let (p,q) = f(p_size,q_size)?;
     let n = p.clone() * q.clone();
 
-    let public_key = PublicKey{e,n};
+    let public_key = PublicKey{n};
     let secret_key = SecretKey{p,q};
     
     return Ok((public_key, secret_key))
@@ -51,8 +49,8 @@ pub fn generate(bit_size: u32) -> Result<(PublicKey,SecretKey),String> {
 
 pub fn encrypt(m: Message, pk: PublicKey) -> Cipher {
     let m_number = number_from_bytes(m);
-    let cipher = m_number.modpow(&pk.e, &pk.n);
-    return cipher.to_bytes_be()
+    let cipher = m_number.modpow(&BigUint::from(e), &pk.n);
+    return bytes_from_number(cipher)
 }
 
 fn number_from_bytes(m: Message) -> BigUint {
@@ -63,6 +61,18 @@ fn bytes_from_number(c: BigUint) -> Cipher {
     return c.to_bytes_be()
 }
 
-pub fn decrypt(c: Cipher) -> Message {
-    todo!()
+pub fn decrypt(c: Cipher, sk: SecretKey) -> Message {
+    let c_number = number_from_bytes(c);
+    let d = create_d(&sk.p,&sk.q);
+
+    let message = c_number.modpow(&d, &(sk.p * sk.q));
+    return bytes_from_number(message);
+}
+
+fn create_d(p: &BigUint,q: &BigUint) -> BigUint {
+    let big_one = BigUint::new(vec![1]);
+    let modulus = (p - &big_one) * (q - &big_one);
+
+    let d = BigUint::from(e) % modulus; //Alex; 22/01/2023; TODO: do modular inverse d = e^(-1) mod (p - 1)(q - 1) here!
+    return d;
 }
