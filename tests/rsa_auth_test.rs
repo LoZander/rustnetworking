f8544ea65a162a4fe5d4b604ea71c66ac94291d9
Alexander D. Troelsen <LoZander@outlook.com>
2023-01-27 17:59:32 +0100

    Signature and verification tests

---
+++ tests/rsa_auth_test.rs
@@ -0,0 +1,28 @@
+use rustnetworking::rsa::{authenticity::{self as auth, Signature, Verification}, keygen, confidentiality::Message};
+
+#[test]
+fn verification_of_correct_message_sign_pair_accepts() -> Result<(),String> {
+    let (pk,sk) = keygen(2048)?;
+    let m: Message = "This is a test".into();
+    let s: Signature = auth::sign(m.clone(), sk)?;
+    let v: Verification = auth::verify(m, s, pk);
+    
+    match v {
+        Verification::Reject => Err("verification failed".into()),
+        Verification::Accept => Ok(())
+    }
+}
+
+#[test]
+fn verification_of_message_modified_by_adversary_rejects() -> Result<(),String> {
+    let (pk,sk) = keygen(2048)?;
+    let m: Message = "This is a test, once again".into();
+    let s: Signature = auth::sign(m, sk)?;
+    let v: Verification = auth::verify("This is a different message injected by an adversary >:D", s, pk);
+
+    match v {
+        Verification::Accept => Err("verification didn't fail, even though the message didn't match the signature".into()),
+        Verification::Reject => Ok(())
+    }
+
+}
